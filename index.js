f37195e7073c70a6e9adbd19b936e696e76eb91a
Abhay-N-J <abhayjoshi029@gmail.com>
2022-11-30 19:35:40 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 // app.use(cors({ origin: "http://localhost:3000" }));
 app.use(cors());
 
+app.get('/',(req, res) => {
+    res.send("Home of server")
+})
+
+
 app.post('/login', async (req, res, next) => {
     // console.log(req.body);
     try {
